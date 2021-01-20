d5b404bb700a0ca731b09df8d136fa688b1fa35c
Murgany <raida103006@yahoo.com>
2021-01-20 05:12:29 +0200

    project rebuilt

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
